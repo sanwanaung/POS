55d578e32d3968965302e00de0c65c08ed6854f6
San Wan Aung <sanwanaung400042@gmail.com>
2023-10-09 10:28:53 +0630

    feat: intial data modeling

---
+++ src/components/design.ts
@@ -0,0 +1,29 @@
+/*
+        *********** TABLES **************
+1. products
+2. categories
+3. users
+4. orders
+5. icons --> name(id)
+
+
+1. Product 
+    id, name, img, price, stock, regular price, discount, tags(fruits, meat) -> keywords;
+
+2. Category
+    id, name, iconId(name), 
+
+3. Products_Categories
+    id , productId, categoryId,
+
+4. User
+    id, firstname, lastname, email, password, phone, role(enum), 
+
+5. icons
+    icons_name();
+
+
+
+
+
+*/
